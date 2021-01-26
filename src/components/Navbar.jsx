1231e45a0f4b12451444ca33d4f7d906ff4e91c0
unknown <bshara23@gmail.com>
2021-01-26 10:36:12 +0200

    added current user name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import {Icon, InlineIcon} from '@iconify/react';
-import {connect} from 'react-redux';
+import {connect, useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
-import { setCurrentUser } from '../data/Global';
-
+import {currentUser, setCurrentUser} from '../data/Global';
 
 function Navbar2(props) {
   const dispatch = useDispatch();
+  const user = useSelector(currentUser);
 
   const onLogOut = () => {
     dispatch(setCurrentUser(null));
@@ -21,28 +21,16 @@ function Navbar2(props) {
         expand="lg"
         className="navbarc myNavBar shadow"
       >
-        <Navbar.Brand id="gradingSystem" href="/">
-          Grading System
-        </Navbar.Brand>
+        <Navbar.Brand id="gradingSystem">Grading System</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/LogIn">Log In</Nav.Link>
-            <Nav.Link href="/Grades">Grades</Nav.Link>
-
-            {/* <Nav.Link href="/Sales">SALES</Nav.Link> */}
-          </Nav>
           <Nav>
-            {/* <Nav.Link eventKey={2} href="/ContactPagee">
-              <div id="contactContainer">
-                <Icon className="icons" icon={handbagIcon} />
-                CART{" "}
-                <Badge variant="primary">{this.props.cartItemsCount}</Badge>
-              </div>
-            </Nav.Link> */}
-            <Nav.Link eventKey={2} href="/profile" onClick={onLogOut}>
-              <div>Bshara</div>
-            </Nav.Link>
+            {user && (
+              <Nav.Link eventKey={2}>
+                <div>{`${user.fname} ${user.lname}`}</div>
+              </Nav.Link>
+            )}
+
             <Nav.Link eventKey={2} href="/" onClick={onLogOut}>
               <div>Log Out</div>
             </Nav.Link>
